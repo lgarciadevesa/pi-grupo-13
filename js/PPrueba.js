d26fe62e52ea2e5cb58ef57efa91142f22e86a75
jmarot-udesa <jmarot@udesa.edu.ar>
2025-11-16 19:30:00 -0300

    js index corregido

--- js/PPrueba.js
+++ js/PPrueba.js
@@ -1,71 +1,93 @@
 const productos = 'https://dummyjson.com/products';
+const beauty = 'https://dummyjson.com/products/category/beauty?limit=10';
+const fragrances = 'https://dummyjson.com/products/category/fragrances?limit=10';
+
 
 const beautyContainer = document.querySelector('.BeautyList');
 const allContainer = document.querySelector('.ProductList');
+
 
 if (!beautyContainer || !allContainer) {
     console.error('No se encontraron los contenedores .BeautyList o .ProductList en el DOM');
 } else {
 
+    
     fetch(productos)
-        .then(function (res) {
-
+        .then(function(res) {
             return res.json();
         })
-        .then(function (data) {
-
+        .then(function(data) {
 
             const products = data.products || [];
 
+            
+            fetch(beauty)
+                .then(function(res) {
+                    return res.json();
+                })
+                .then(function(dataBeauty) {
 
-            const beautyProducts = [];
-            for (let i = 0; i < products.length; i++) {
-                const p = products[i];
+                    const beautyProducts = dataBeauty.products || [];
 
-                if (p.category) {
-                    let cat = p.category.toLowerCase();
-                    if (cat === 'beauty' || cat === 'fragrances') {
-                        beautyProducts.push(p);
-                    }
-                }
+                  
+                    fetch(fragrances)
+                        .then(function(res) {
+                            return res.json();
+                        })
+                        .then(function(dataFragrances) {
 
-            }
+                            const fragranceProducts = dataFragrances.products || [];
 
-            function renderList(container, list) {
-                if (!list || list.length === 0) {
-                    container.innerHTML = '<p>No se encontraron productos.</p>';
-                    return;
-                }
+                            
+                            const combinedList = beautyProducts.concat(fragranceProducts);
 
-                let html = '';
+                            
+                            renderList(beautyContainer, combinedList);
+                            renderList(allContainer, products);         
 
-                for (let i = 0; i < list.length; i++) {
-                    const p = list[i];
-                    const img = p.thumbnail ;
+                        })
+                        .catch(function(error) {
+                            console.log('Error al traer frangrances: ' + error);
+                        });
 
-                    html += `
-           <article class="product-card">
-    <img src="${img}" alt="${p.title}">
-    <p class="title">${p.title}</p>
-    <p class="price">Price: $${p.price}</p>
-      <p class="description">${p.description}</p>
+                })
+                .catch(function(error) {
+                    console.log('Error al traer beauty: ' + error);
+                });
 
-    <a href="./product.html" class="verDetalles">
-      Ver detalles
-    </a>
-  </article>
-          `;
-                }
-
-                container.innerHTML = html;
-            }
-
-            renderList(beautyContainer, beautyProducts);
-            renderList(allContainer, products);
         })
-        .catch(function (error) {
-            console.log('Error: ' + error);
+        .catch(function(error) {
+            console.log("Error al traer productos: " + error);
         });
+}
 
-    console.log(productos);
+
+function renderList(container, list) {
+
+    if (!list || list.length === 0) {
+        container.innerHTML = '<p>No se encontraron productos.</p>';
+        return;
+    }
+
+    let html = '';
+
+    for (let i = 0; i < list.length; i++) {
+        const p = list[i];
+        const img = p.thumbnail || '';
+
+        html += `
+        <article class="product-card">
+            <img src="${img}" alt="${p.title}">
+            <p class="title">${p.title}</p>
+            <p class="price">Precio: $${p.price}</p>
+            <p class="description">${p.description}</p>
+
+            <a href="./product.html?id=${p.id}" class="verDetalles">
+                Ver detalles
+            </a>
+        </article>
+        `;
+    }
+
+    container.innerHTML = html;
 }
